2b1993fdc55cf7ce5bcccd92c751c46dc5d22370
jdnfkf <549829599@qq.com>
2026-09-24 22:26:13 +0800

    Delete 加载器.lua

--- 加载器.lua
+++
@@ -1,8 +0,0 @@
-local scripts = {
-    [104841616983113] = "https://raw.githubusercontent.com/jdnfkf/U00pkid/main/ANSNv9.5.lua"
-}
-
-local url = scripts[game.PlaceId]
-if url then
-    loadstring(game:HttpGet(url))()
-end
