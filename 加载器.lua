edcaf37c050a8adb42c8768990db0f3adfafb13d
jdnfkf <549829599@qq.com>
2026-09-24 22:15:55 +0800

    Add files via upload

--- 加载器.lua
+++ 加载器.lua
@@ -1,5 +1,5 @@
 local scripts = {
-    [104841616983113] = "https://raw.githubusercontent.com/jdnfkf/U00pkid/main/ANSNv9(%C3%BD%C3%BD%C3%BD)-obfuscated.lua"
+    [104841616983113] = "https://raw.githubusercontent.com/jdnfkf/U00pkid/main/ANSNv9.5.lua"
 }
 
 local url = scripts[game.PlaceId]
